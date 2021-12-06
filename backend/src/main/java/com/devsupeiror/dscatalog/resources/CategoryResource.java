2a0e8a207be53f85735b7287ebd2afd0b73ce9fd
JefFischer <83618550+JefFischer@users.noreply.github.com>
2021-12-05 21:10:42 -0300

    Find Category by id , exception handling

--- backend/src/main/java/com/devsupeiror/dscatalog/resources/CategoryResource.java
+++ backend/src/main/java/com/devsupeiror/dscatalog/resources/CategoryResource.java
@@ -1,9 +1,11 @@
 package com.devsupeiror.dscatalog.resources;
+
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -15,11 +17,17 @@ import com.devsupeiror.dscatalog.services.CategoryService;
 public class CategoryResource {
 	@Autowired
 	private CategoryService service;
-	
+
 	@GetMapping
 	public ResponseEntity<List<CategoryDTO>> findAll() {
-		List<CategoryDTO>list =service.findAll();
-		
+		List<CategoryDTO> list = service.findAll();
 		return ResponseEntity.ok().body(list);
+	}
+
+	@GetMapping(value = "/{id}")
+	public ResponseEntity<CategoryDTO> findById(@PathVariable Long id) {
+		CategoryDTO dto = service.findByID(id);
+	
+		return ResponseEntity.ok().body(dto);
 	}
 }
---
+++ backend/src/main/java/com/devsupeiror/dscatalog/services/exceptions/EntityNotFoundException.java
@@ -0,0 +1,9 @@
+package com.devsupeiror.dscatalog.services.exceptions;
+
+public class EntityNotFoundException extends RuntimeException {
+	private static final long serialVersionUID = 1l;
+	
+	public EntityNotFoundException (String msg) {
+		super(msg);
+	}
+}
